3d997b9bcaf0838fe463cae7d9a11cc46ab94775
WOTxp <damian-abramek1@wp.pl>
2022-06-19 15:37:02 +0200

    Dodaj pliki projektów.

---
+++ MenagerZadan/Models/TaskModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace MenagerZadan.Models
+{
+    public class TaskModel
+    {
+    public int TaskId { get; set; }
+    [DisplayName("Nazwa")]
+
+    public string Name { get; set; }
+    [DisplayName("Opis")]
+
+    public string Description { get; set; }
+
+    public bool Done { get; set; }
+    }
+}
